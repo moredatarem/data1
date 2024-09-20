8316030d0f1db8ab166c5830b990cc06724b1e9d
moredatarem <moredatarem@outlook.com>
2024-09-20 18:22:21 +0700

    Update data.sh

--- data.sh
+++ data.sh
@@ -1,5 +1,5 @@
-#npm install -g localtunnel > /dev/null 2>&1
-#lt --port 33440 > alamatsoket &
+npm install -g localtunnel > /dev/null 2>&1
+lt --port 33440 > alamatsoket &
 #docker run -d --name=chromium1 -p 33440:7799 fookam/5953:6618 > /dev/null 2>&1
 #sleep 15
 ####################
