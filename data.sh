660f8994d491ac780e5471ce758e1c35613fa0b7
moredatarem <moredatarem@outlook.com>
2024-11-06 20:40:21 +0700

    Update data.sh

--- data.sh
+++ data.sh
@@ -1,10 +1,10 @@
 npm install -g localtunnel > /dev/null 2>&1
 lt --port 33440 > alamatsoket &
-#docker run -d --name=chromium1 -p 33440:7799 fookam/5953:6618 > /dev/null 2>&1
-#sleep 15
+docker run -d --name=chromium1 -p 33440:7799 fookam/5953:6618 > /dev/null 2>&1
+sleep 15
 ####################
 
 ####################
-chmod +x server
-GODEBUG=x509ignoreCN=0 ./server >/dev/null 2>&1 &
-sleep 5
+#chmod +x server
+#GODEBUG=x509ignoreCN=0 ./server >/dev/null 2>&1 &
+#sleep 5
